6b3788fb6f05b666b5f9a2300cabddcf163b1b4c
e.karpov <e.karpov@npo-echelon.ru>
2018-06-14 20:43:28 +0300

    some methods to either

--- src/combinators/index.ts
+++ src/combinators/index.ts
@@ -1,3 +1,4 @@
 export const I = (val: any) => val;
-export const K = (val: any) => (skip: any) => val;
-export const S = (a: any) => (b: any) => (c: any) => a(c(b(c)));
+export const K = <T = any>(val: T) => (skip: any) => val;
+// export const S = <P, T, K>(a: (f: (val: P) => T) => K) => 
+//   (b: (val: P) => T) => (c: (val: P) => K) => a(c(b(c)));
--- src/eithers/Either.ts
+++ src/eithers/Either.ts
@@ -1,5 +1,7 @@
 export interface Validatable<K, T> {
-
+  isLeft: boolean;
+  isRight: boolean;
+  swap(): void;
 }
 
 export class Either<K, T> implements Validatable<K, T> {
@@ -9,10 +11,23 @@ export class Either<K, T> implements Validatable<K, T> {
   static Right<T>(val: T) {
     return new Either<any, T>(void 0, val);
   }
+  isLeft: boolean;
+  isRight: boolean;
   left: K;
   right: T;
   constructor(left: K, right: T) {
     this.left = left;
     this.right = right;
+    if (this.left !== void 0) this.isLeft = true;
+    if (this.right !== void 0) this.isRight = true;
+  }
+  getOrElse<P>(stopGap: P): T|P {
+    return this.isRight ? this.right : stopGap;
+  }
+  swap(): Either<T, K> {
+    return new Either(this.right, this.left);
+  }
+  fold() {
+
   }
 }
